e54275f1bd0d62e5c7be0b47b5241fed299022a2
nextBillyonair <nextbillyonair@gmail.com>
2021-08-06 11:21:26 -0400

    Updates

---
+++ precache-manifest.d3f6d8f5f79d650b86dad0fd645f46c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b62e1b0346623754c6c0f25336f884d",
+    "url": "/react-go-game/index.html"
+  },
+  {
+    "revision": "ab776bc47e13a7fbc0f7",
+    "url": "/react-go-game/static/css/main.310f29a1.chunk.css"
+  },
+  {
+    "revision": "f45c667a3adba2d844a9",
+    "url": "/react-go-game/static/js/2.5bb9c039.chunk.js"
+  },
+  {
+    "revision": "e6191615acbaa88870aef4cd0368e4ec",
+    "url": "/react-go-game/static/js/2.5bb9c039.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab776bc47e13a7fbc0f7",
+    "url": "/react-go-game/static/js/main.fcb8624a.chunk.js"
+  },
+  {
+    "revision": "42b9a84b384d0b388da7",
+    "url": "/react-go-game/static/js/runtime-main.99b18245.js"
+  }
+]);
